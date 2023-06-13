584abe44a288e61bc5c1f469fcf8dc8f460129d1
vladian95 <kvankvan1995@gmail.com>
2023-06-13 18:05:50 +0300

    bg home

---
+++ components/HomeBg/HomeBg.jsx
@@ -0,0 +1,26 @@
+import './homeBg.scss';
+
+const HomeBg = () => {
+  return (
+    <div className="homeBG">
+      <ul class="circles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default HomeBg;
