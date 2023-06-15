e88057b1dcf2d9651ae3b39803088781e90f7f4b
vladian95 <kvankvan1995@gmail.com>
2023-06-15 15:19:26 +0300

    page transition

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,21 @@
+'use client';
 import './globals.scss';
 import { Inter } from 'next/font/google';
+import { AnimatePresence } from 'framer-motion';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'Home',
-};
+// export const metadata = {
+//   title: 'Home',
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main>{children}</main>
+        <AnimatePresence>
+          <main>{children}</main>
+        </AnimatePresence>
       </body>
     </html>
   );
--- app/website/page.jsx
+++ app/website/page.jsx
@@ -1,12 +1,30 @@
-export const metadata = {
-  title: 'website',
-};
+'use client';
+import { motion } from 'framer-motion';
+import './page.scss';
+
+// export const metadata = {
+//   title: 'website',
+// };
 
 const Website = () => {
   return (
-    <>
-      <div>WEBSITE</div>
-    </>
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 2, ease: 'easeOut' }}
+      exit={{ opacity: 1 }}
+      className="site__wrapper"
+    >
+      <div className="site">
+        <motion.h1
+          animate={{ y: 0 }}
+          initial={{ y: '100%' }}
+          transition={{ delay: 0.5, duration: 0.5 }}
+        >
+          WEBSITE
+        </motion.h1>
+      </div>
+    </motion.section>
   );
 };
 
